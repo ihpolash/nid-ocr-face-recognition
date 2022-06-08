505450ce64428c72edca34c1ab3bb1c7545cb2dd
ihpolash <ismailhossainpolash@gmail.com>
2022-06-08 20:54:46 +0600

    change client

--- client.py
+++ client.py
@@ -86,40 +86,40 @@ def face_detect(image):
 
 def card_ocr(image):
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    try:
+    # try:
 
-        image_path = default_storage.save("tmp/temp.jpg", ContentFile(image.read()))
-        tmp_file = os.path.join(settings.MEDIA_ROOT, image_path)
+    image_path = default_storage.save("tmp/temp.jpg", ContentFile(image.read()))
+    tmp_file = os.path.join(settings.MEDIA_ROOT, image_path)
 
-        response = run(
-            weights=ROOT / 'best_nid.pt',  # model.pt path(s)
-            source=ROOT / tmp_file,  # file/dir/URL/glob, 0 for webcam
-            data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-            imgsz=(416, 416),  # inference size (height, width)
-            conf_thres=0.25,  # confidence threshold
-            iou_thres=0.45,  # NMS IOU threshold
-            max_det=1000,  # maximum detections per image
-            device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-            view_img=False,  # show results
-            save_txt=False,  # save results to *.txt
-            save_conf=False,  # save confidences in --save-txt labels
-            save_crop=True,  # save cropped prediction boxes
-            nosave=False,  # do not save images/videos
-            classes=None,  # filter by class: --class 0, or --class 0 2 3
-            agnostic_nms=False,  # class-agnostic NMS
-            augment=False,  # augmented inference
-            visualize=False,  # visualize features
-            update=False,  # update all models
-            project=ROOT / 'runs/detect',  # save results to project/name
-            name='exp',  # save results to project/name
-            exist_ok=False,  # existing project/name ok, do not increment
-            line_thickness=3,  # bounding box thickness (pixels)
-            hide_labels=False,  # hide labels
-            hide_conf=False,  # hide confidences
-            half=False,  # use FP16 half-precision inference
-            dnn=False,  # use OpenCV DNN for ONNX inference
-        )
-    except:
-        response = {"result": "OCR Unsuccessful!"}
+    response = run(
+        weights=ROOT / 'best_nid.pt',  # model.pt path(s)
+        source=ROOT / tmp_file,  # file/dir/URL/glob, 0 for webcam
+        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
+        imgsz=(416, 416),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        view_img=False,  # show results
+        save_txt=False,  # save results to *.txt
+        save_conf=False,  # save confidences in --save-txt labels
+        save_crop=True,  # save cropped prediction boxes
+        nosave=False,  # do not save images/videos
+        classes=None,  # filter by class: --class 0, or --class 0 2 3
+        agnostic_nms=False,  # class-agnostic NMS
+        augment=False,  # augmented inference
+        visualize=False,  # visualize features
+        update=False,  # update all models
+        project=ROOT / 'runs/detect',  # save results to project/name
+        name='exp',  # save results to project/name
+        exist_ok=False,  # existing project/name ok, do not increment
+        line_thickness=3,  # bounding box thickness (pixels)
+        hide_labels=False,  # hide labels
+        hide_conf=False,  # hide confidences
+        half=False,  # use FP16 half-precision inference
+        dnn=False,  # use OpenCV DNN for ONNX inference
+    )
+    # except:
+    #     response = {"result": "OCR Unsuccessful!"}
 
     return response
